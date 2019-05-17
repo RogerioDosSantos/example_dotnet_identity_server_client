3153fb533802f33ddec3a87496f80d152b70cd5f
Roger <roger.santos.main@gmail.com>
2019-05-17 09:42:13 -0500

    Added the client

---
+++ src/client/Startup.cs
@@ -0,0 +1,79 @@
+﻿
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System;
+
+namespace client
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v0.0.0",
+                    Title = "Identity Server Example (client)",
+                    // All Configuration below are optional
+                    Description = "Example of the Identity Server (Client Implementation)",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "Roger Santos",
+                        Email = string.Empty,
+                        Url = "https://github.com/RogerioDosSantos/example_dotnet_identity_server_client"
+                    },
+                    License = new License
+                    {
+                        Name = "MIT",
+                        Url = "https://opensource.org/licenses/MIT"
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+          // Enable middleware to serve generated Swagger as a JSON endpoint.
+          app.UseSwagger();
+          // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+          // specifying the Swagger JSON endpoint.
+          app.UseSwaggerUI(c =>
+          {
+              c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+              //c.RoutePrefix = string.Empty;
+          });
+          app.UseMvc();
+        }
+    }
+}
