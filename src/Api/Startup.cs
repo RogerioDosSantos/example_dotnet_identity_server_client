0c2f2fbd5a41b9c35a5c15155c5f369cb30b4257
Roger <roger.santos.main@gmail.com>
2019-05-20 15:07:45 -0500

    Added example from walktrough

---
+++ src/Api/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore()
+            .AddAuthorization()
+            .AddJsonFormatters();
+
+            //Adds the authentication services to DI and configures "Bearer" as the default scheme
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", options =>
+                {       
+                    options.Authority = "http://localhost:5000"; //IdentityServer Address
+                    options.RequireHttpsMetadata = false;
+                    options.Audience = "api1";
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            //Adds the authentication middleware to the pipeline so authentication will be performed automatically on every call into the host
+            app.UseAuthentication();
+
+            app.UseMvc();
+        }
+    }
+}
---
+++ src/Client/Program.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Program
+    {
+        private static async Task Main()
+        {
+            // discover endpoints from metadata
+            var client = new HttpClient();
+
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000"); //URL of the IdentityServer
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            // request token
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api1"
+            });
+            
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            // call api
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken); //Use HTTP Authorization header to send the access token to the API
+
+            try
+            {
+                var response = await apiClient.GetAsync("http://localhost:5001/identity"); //API address
+                if (!response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine(response.StatusCode);
+                }
+                else
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(JArray.Parse(content));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error Accessing the API. Source: " + ex.Source + " ; Description: " + ex.Message);
+            }
+
+            
+        }
+    }
+}
