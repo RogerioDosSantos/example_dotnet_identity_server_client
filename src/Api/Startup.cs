1eaf4e7a2353736d0642dde694411ff509ff3fb8
Roger <roger.santos.main@gmail.com>
2019-05-22 16:42:43 -0500

    Added proxy configuration

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Api
@@ -15,20 +13,32 @@ namespace Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            string proxyUrl = Environment.GetEnvironmentVariable("proxy_url");
+            string identityServerUrl = Environment.GetEnvironmentVariable("identity_server_url");
+            string identityServerAudience = Environment.GetEnvironmentVariable("identity_server_audience");
+            Console.WriteLine("\n00 - Current Configuration:");
+            Console.WriteLine("- Proxy URL (proxy_url): " + proxyUrl);
+            Console.WriteLine("- Identity Server URL (identity_server_url): " + identityServerUrl);
+            Console.WriteLine("- Identity Server Audience (identity_server_audience): " + identityServerAudience);
+
             services.AddMvcCore()
             .AddAuthorization()
             .AddJsonFormatters();
-
+           
             //Adds the authentication services to DI and configures "Bearer" as the default scheme
-            string identityServerUrl = Environment.GetEnvironmentVariable("identity_server_url");
-            Console.WriteLine("\n00 - Current Configuration:");
-            Console.WriteLine("- Identity Server URL (identity_server_url): " + identityServerUrl);
             services.AddAuthentication("Bearer")
                 .AddJwtBearer("Bearer", options =>
                 {                    
                     options.Authority = identityServerUrl; //IdentityServer Address
                     options.RequireHttpsMetadata = false;
-                    options.Audience = "api1";
+                    options.Audience = identityServerAudience;
+                    if(proxyUrl != null && proxyUrl != "")
+                    {
+                        options.BackchannelHttpHandler = new HttpClientHandler() //Configure Proxy
+                        {
+                            Proxy = new WebProxy(proxyUrl)
+                        };
+                    }                        
                 });
         }
 
--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -4,6 +4,7 @@
 using IdentityModel.Client;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -27,34 +28,56 @@ namespace Client
                 Console.WriteLine("\n==================\nWaitting next cycle...");
                 await Task.Delay(5000);
 
+                string proxyUrl = Environment.GetEnvironmentVariable("proxy_url");
                 string identityServerUrl = Environment.GetEnvironmentVariable("identity_server_url");
                 string apiUrl = Environment.GetEnvironmentVariable("api_url");
-                bool requireHttps = Environment.GetEnvironmentVariable("requite_https") == "true";
-                string clientId = Environment.GetEnvironmentVariable("client_id");
-                string clientSecret = Environment.GetEnvironmentVariable("client_secret");
-                string scope = Environment.GetEnvironmentVariable("scope");
+                bool identityServerRequireHttps = Environment.GetEnvironmentVariable("identity_server_require_https") == "true";
+                string identityServerClientId = Environment.GetEnvironmentVariable("identity_server_client_id");
+                string identityServerClientSecret = Environment.GetEnvironmentVariable("identity_server_client_secret");
+                string identityServerScope = Environment.GetEnvironmentVariable("identity_server_scope");                
                 Console.WriteLine("\n00 - Current Configuration:");
+                Console.WriteLine("- Proxy URL (proxy_url): " + proxyUrl);
                 Console.WriteLine("- Identity Server URL (identity_server_url): " + identityServerUrl);
-                Console.WriteLine("- API URL (api_url): " + identityServerUrl);
-                Console.WriteLine("- Require HTTPS (requite_https): " + requireHttps.ToString());
-                Console.WriteLine("- Client ID (client_id): " + clientId);
-                Console.WriteLine("- Client Secret (client_secret): " + clientSecret);
-                Console.WriteLine("- Scope (scope): " + scope);
+                Console.WriteLine("- Identity Server Scope (identity_server_scope): " + identityServerScope);
+                Console.WriteLine("- Identity Server Client ID (identity_server_client_id): " + identityServerClientId);
+                Console.WriteLine("- Identity Server Client Secret (identity_server_client_secret): " + identityServerClientSecret);
+                Console.WriteLine("- API URL (api_url): " + apiUrl);
+                Console.WriteLine("- Require HTTPS (require_https): " + identityServerRequireHttps.ToString());
 
+                //define a HTTPHandler with Proxy configuration
+                HttpClientHandler httpHandlerWithProxy = null;
+                if (proxyUrl != null && proxyUrl != "")
+                {
+                    Console.WriteLine("\n00 - Creating Proxy Handler: " + proxyUrl);
+                    httpHandlerWithProxy = new HttpClientHandler
+                    {
+                        //UseDefaultCredentials = true
+                        UseProxy = true,
+                        PreAuthenticate = true,
+                        Proxy = new WebProxy()
+                        {
+                            Address = new Uri(proxyUrl), //proxy address
+                            UseDefaultCredentials = false,
+                            Credentials = System.Net.CredentialCache.DefaultNetworkCredentials
+                        }
+                    };
+                }
+                
                 // discover endpoints from metadata
                 Console.WriteLine("\n01 - Discovering Entrypoints:");
                 DiscoveryResponse discoveryResponse = null;
-                if (requireHttps)
+                if (identityServerRequireHttps)
                 {
                     Console.WriteLine("- Using HTTPS");
-                    var discoveryClient = new HttpClient();
+                    var discoveryClient = new HttpClient(httpHandlerWithProxy);
                     discoveryResponse = await discoveryClient.GetDiscoveryDocumentAsync(identityServerUrl); //URL of the IdentityServer
                 }
                 else
                 {
-                    Console.WriteLine("- Using HTTPS");
-#pragma warning disable CS0618 // Type or member is obsolete                
-                    var discoveryClient = new DiscoveryClient(identityServerUrl);
+                    Console.WriteLine("- Not Using HTTPS");
+#pragma warning disable CS0618 // Type or member is obsolete                        
+                    var discoveryClient = httpHandlerWithProxy != null ? new DiscoveryClient(identityServerUrl, httpHandlerWithProxy) : new DiscoveryClient(identityServerUrl);
+                    //var discoveryClient = new DiscoveryClient(identityServerUrl);
 #pragma warning restore CS0618 // Type or member is obsolete
                     discoveryClient.Policy.RequireHttps = false;
                     discoveryResponse = await discoveryClient.GetAsync();
@@ -68,13 +91,15 @@ namespace Client
 
                 // request token
                 Console.WriteLine("\n02 - Requesting Tokens:");
-                var client = new HttpClient();
+                var client = httpHandlerWithProxy != null ? new HttpClient(httpHandlerWithProxy) : new HttpClient();
+
+                // Requesting a token using the client_credentials Grant Type
                 var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
                 {
                     Address = discoveryResponse.TokenEndpoint,
-                    ClientId = clientId,
-                    ClientSecret = clientSecret,
-                    Scope = scope
+                    ClientId = identityServerClientId,
+                    ClientSecret = identityServerClientSecret,
+                    Scope = identityServerScope
                 });
 
                 if (tokenResponse.IsError)
@@ -84,6 +109,7 @@ namespace Client
                 }
 
                 Console.WriteLine(tokenResponse.Json);
+
                 // call api                
                 var apiClient = new HttpClient();
                 Console.WriteLine("\n03 - Calling API:");
@@ -93,7 +119,7 @@ namespace Client
                     var response = await apiClient.GetAsync(apiUrl + "/identity"); //API address
                     if (!response.IsSuccessStatusCode)
                     {
-                        Console.WriteLine("Error: " + response.StatusCode);
+                        Console.WriteLine("Error: " + response.StatusCode);                        
                         continue;
                     }
                     else
